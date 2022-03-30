e64d51fcc8ee29983771968b12e7b5de48abaa1e
Janainaf <figueira.janaina@gmail.com>
2022-03-30 12:10:13 +0200

    added exception

--- guessing_game.py
+++ guessing_game.py
@@ -11,44 +11,58 @@ NOTE: If you prefer to work locally on your own computer, you can totally do tha
 import random
 import statistics
 
+
    
 def start_game():
   
-  
+  print("* Welcome to Guess THAT Number Game *")
+  secretNumber =  random.randrange(0, 100)
   attemptNumber = [] 
-  
+  attempts = 1
+
   while True:
-    print("**** Welcome to Guess THAT Number Game ****")
-    secretNumber =  random.randrange(0, 100)
-    guessedNumber = int(input("Please, guess a number between 1 to 100 -> "))
-    attempts = 1
-    while guessedNumber != secretNumber:
-              if guessedNumber > secretNumber:
-                attempts +=1
-                print ("It's lower. Try again!")
-                guessedNumber = int(input("Please, guess another number: "))
-              
-              elif guessedNumber < secretNumber: 
-                attempts +=1
-                print ("It's higher. Try again!")
-                guessedNumber = int(input("Please, guess another number: "))
-              
+    
+    try:
+      guessedNumber = int(input("Please, guess a number between 1 to 100 -> "))
+      
+    except (ValueError, UnboundLocalError): 
+      print("Oops!  That was no valid number.  Try again...")
+    
     else:
-        print("***** You Got it! ******")
-        print("It took you " + str(attempts) + " attempts to get the correct number" )
-        attemptNumber.append(attempts)
-        print ("*** Overall Statistics ***" )
-        print (f"MEAN: {str(statistics.mean(attemptNumber))}" )
-        print(f"MEDIAN: {str(statistics.median(attemptNumber))}" )
-        print(f"MODE: {str(statistics.mode(attemptNumber))}" )
+      
+      
+      print (secretNumber)
+      # while guessedNumber != secretNumber:
+      if guessedNumber > secretNumber:
+         attempts +=1
+         print ("It's lower. Try again!")
+         #guessedNumber = int(input("Please, guess another number: "))
+         continue
+              
+      elif guessedNumber < secretNumber: 
+            attempts +=1
+            print ("It's higher. Try again!")
+            continue
+    
+              
+      else:
+            print("* You Got it! **")
+            print("It took you " + str(attempts) + " attempts to get the correct number" )
+            attemptNumber.append(attempts)
+            print ("* Overall Statistics *" )
+            print (f"MEAN: {str(statistics.mean(attemptNumber))}" )
+            print(f"MEDIAN: {str(statistics.median(attemptNumber))}" )
+            print(f"MODE: {str(statistics.mode(attemptNumber))}" )
 
-        restart = input("Would like to play again? [Y/N] ? ")
-    if restart != "N":
-       continue
-
-    else:
-      print("Thanks for playing" )
-      exit()
+            restart = input("Would like to play again? [Y/N] ? ")
+            if restart != "N":
+              secretNumber =  random.randrange(0, 100)
+              attempts = 1
+              continue
+        
+            else:
+               print("Thanks for playing" )
+               exit()
 
         
 start_game()
